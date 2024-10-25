96bbf5378dccdfc6d06405d66bac64a39be014d1
root <root@estadistica>
2024-10-25 19:06:52 +0200

    estilo solucionado

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import './style/App.css'
-/* import UserPage from './pages/UserPage.tsx' */
-import {Song} from './components/Song.tsx'
+import UserPage from './pages/UserPage.tsx'
+/* import {Song} from './components/Song.tsx' */
 
 function App() {
 
   return (
     <>
-    <Song id={15}/>
+    <UserPage/>
     </>
   )
 }
