9a4a218f4f96730ded9212163aef6899f74a89d4
santiichaya <146816651+santiichaya@users.noreply.github.com>
2024-10-25 18:59:42 +0200

    Merge remote-tracking branch 'origin/master' into santi Merge para solucionar problemas

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,22 @@ function App() {
 
   return (
     <>
+<<<<<<< Updated upstream
     <UserPage/>
+=======
+    <Song id={10}/>
+    <Song id={11}/>
+    <Song id={12}/>
+    <Song id={13}/>
+    <Song id={14}/>
+    <Song id={9}/>
+    <Song id={8}/>
+    <Song id={7}/>
+    <Song id={6}/>
+    <Song id={5}/>
+    <Song id={4}/>
+    <Song id={3}/>
+>>>>>>> Stashed changes
     </>
   )
 }
