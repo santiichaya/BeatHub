c768e23bb22a1b7392fdb63c887367f7cb3353b7
Melen <96883639+cmelendo@users.noreply.github.com>
2024-10-26 13:21:27 +0200

    Todo implementado y arreglado, directorios duplicados organiazdos

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,9 @@
 import './style/App.css'
-import UserPage from './pages/UserPage.tsx'
 /* import {Song} from './components/Song.tsx' */
-import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import SearchPage from './pages/SearchPage';
+import UserPage from './pages/UserPage.tsx';
 
 function App() {
 
@@ -14,11 +13,13 @@ function App() {
       <nav>
         <Link to="/">Inicio</Link>
         <Link to="/search">Buscar Canciones</Link>
+        <Link to="/userpage">Perfil</Link>
       </nav>
 
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/search" element={<SearchPage />} />
+        <Route path="/userpage" element={<UserPage />} />
       </Routes>
     </Router>
     </>
