7c14bfdc9369400e786367a6d05c0b6509d6e55e
root <root@estadistica>
2024-10-27 10:52:31 +0100

    estilo

---
+++ src/Components/ContainerPlayList.tsx
@@ -0,0 +1,21 @@
+import { Playlist } from "./Playlist";
+import data from "../data/data.json";
+
+type ContainerPlayListProp = {
+    ids: number[];
+}
+
+export function ContainerPlayList({ ids }: ContainerPlayListProp) {
+    const playlists = data.playlists;
+
+    return (
+        <>
+            <div className="artists-section">
+                {ids.map((v) => (
+                    v == playlists[v - 1].id ? <Playlist key={playlists[v - 1].id} id={playlists[v - 1].id}/> : null
+                ))}
+            </div>
+        </>
+    )
+
+}
