07ddad1dac4c6a06b635bc3514b89cbeb2016226
santiichaya <146816651+santiichaya@users.noreply.github.com>
2024-10-27 08:44:54 +0100

    Add files via upload

---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const HomePage: React.FC = () => {
+  return (
+    <div>
+      <h1>Bienvenido a la página de inicio</h1>
+    </div>
+  );
+};
+
+export default HomePage;
