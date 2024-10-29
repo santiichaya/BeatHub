92a37c3cb628d099cfe90fce829cecb0d492f3a9
Carlos Melendo Bojo <carlos.melendo.bojo@gmail.com>
2024-10-29 10:16:09 +0100

    correciones

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,28 @@
-import './style/App.css'
+import "./style/App.css";
 /* import {Song} from './components/Song.tsx' */
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
-import HomePage from './pages/HomePage';
-import SearchPage from './pages/SearchPage';
-import UserPage from './pages/UserPage.tsx';
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import SearchPage from "./pages/SearchPage";
+import UserPage from "./pages/UserPage.tsx";
 
 function App() {
-
   return (
     <>
-    <Router>
-      <nav>
-        <Link to="/">Inicio</Link>
-        <Link to="/search">Buscar Canciones</Link>
-        <Link to="/userpage">Perfil</Link>
-      </nav>
+      <Router>
+        <nav>
+          <Link to="/">Inicio</Link>
+          <Link to="/search">Buscar Canciones</Link>
+          <Link to="/userpage">Perfil</Link>
+        </nav>
 
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/search" element={<SearchPage />} />
-        <Route path="/userpage" element={<UserPage />} />
-      </Routes>
-    </Router>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/search" element={<SearchPage />} />
+          <Route path="/userpage" element={<UserPage />} />
+        </Routes>
+      </Router>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
