1abb6978234a31732d87e6c4c036481ac2608e6a
root <root@estadistica>
2024-11-20 20:49:20 +0100

    commit 7

--- src/App.tsx
+++ src/App.tsx
@@ -4,25 +4,26 @@ import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import SearchPage from "./pages/SearchPage";
 import UserPage from "./pages/UserPage.tsx";
+import Header from "./components/Header.tsx";
+import { ContainerPlayList } from "./components/ContainerPlayList.tsx";
+import { Display } from "./pages/Display.tsx";
 
 function App() {
   return (
     <>
       <Router>
         <nav>
-          <Link to="/">Inicio</Link>
-          <Link to="/search">Buscar Canciones</Link>
-          <Link to="/userpage">Perfil</Link>
+          <Header />
         </nav>
-
         <Routes>
-          <Route path="/" element={<HomePage />} />
+          <Route path="/UserPage" element={<UserPage/>} />
+          <Route path="/" element={<ContainerPlayList ids={[1,2]} />} />
+          <Route path="/Playlist/:id" element={<Display />} />
           <Route path="/search" element={<SearchPage />} />
-          <Route path="/userpage" element={<UserPage />} />
         </Routes>
       </Router>
     </>
-  );
+  )
 }
 
 export default App;
